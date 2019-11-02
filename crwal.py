759696392c7788f6c6c88ba85b336ecb8e577b1a
신동준 <shindj4613@outlook.com>
2019-11-02 15:52:23 +0900

    90프로 완성

--- crwal.py
+++ crwal.py
@@ -25,24 +25,23 @@ form = section.find("form",{"id":"listpage_form"})
 tbody = form.table.tbody
 recent_notice = tbody.tr
 recent_notice_url = recent_notice.find("td",{"class":"aleft"})
-print(recent_notice_url)
-print()
+#print(recent_notice_url)
+#print("----------------------------------------")
+#
+#print(recent_notice_url.a)
+#print("----------------------------------------")
+#
 
 
+page_number = str(recent_notice_url.a)[65:69]
+notice_title = str(recent_notice_url.a)[139:-12]
+print(notice_title)
+print("-----------------------------------------")
+#print(page_number)
 
-#table = form.find("table",{"class":"table-basic"})
-print(type(section))
-print(type(section.form))
+complete_url = "https://cse.cau.ac.kr/sub05/sub0501.php" + "?dir=bbs&nmode=view&code=oktomato_bbs05&uid=" + page_number + "&search=&keyword=&temp1=&offset=1"
+print(complete_url)
 
-
-
-
-
-
-
-
-#print(type(page_body))
-#print(page_body)
 
 
 
